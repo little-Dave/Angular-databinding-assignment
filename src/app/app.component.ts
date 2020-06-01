353f390beffe578bdfe2cbe86bad06d6f2d26d11
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-01 17:47:45 -0600

    delete gameTimer, update odd and even component styles
    provide same information gameTimer did by accessing numbers array;
    added border radii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  gameTimer: number;
   numbers: number[] = [];
 
   showTime(timer: {elapsedTime: number}) {
-    this.gameTimer = timer.elapsedTime;
     this.numbers.push(timer.elapsedTime);
   }
 
